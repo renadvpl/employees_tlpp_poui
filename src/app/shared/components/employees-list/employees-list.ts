99768d2eb8b393bc2bed55413e1fa83266cec1b9
Renato Silva <renato2920@hotmail.com>
2026-09-07 19:01:03 -0300

    Criação de pesquisa avançada.

--- src/app/shared/components/employees-list/employees-list.ts
+++ src/app/shared/components/employees-list/employees-list.ts
@@ -1,19 +1,57 @@
 import { Component } from '@angular/core';
-import { PoPageAction, PoPageModule } from '@po-ui/ng-components';
+import { PoPageAction } from '@po-ui/ng-components';
+import { PoPageDynamicSearchFilters, PoPageDynamicSearchModule } from '@po-ui/ng-templates';
 
 @Component({
   selector: 'app-employees-list',
-  imports: [PoPageModule],
+  imports: [PoPageDynamicSearchModule],
   templateUrl: './employees-list.html',
   styleUrl: './employees-list.css',
 })
 export class EmployeesList {
   readonly pageActions: PoPageAction[] = [
-    { label: 'Incluir' , action: this.addEmployee.bind(this) }
+    { label: 'Incluir' , action: this.addEmployee.bind(this) , icon: 'an an-plus-square'}
   ];
+
+  readonly advancedSearchFields: PoPageDynamicSearchFilters[] = [
+    {
+      property: 'ra_mat_ge',
+      label: 'Matricula De',
+      divider: 'Matricula',
+      gridColumns: 6
+    },
+    {
+      property: 'ra_mat_le',
+      label: 'Matricula Até',
+      gridColumns: 6
+    },
+    {
+      property: 'ra_cpf_ge',
+      label: 'CPF De',
+      divider: 'CPF',
+      gridColumns: 6
+    },
+    {
+      property: 'ra_cpf_le',
+      label: 'CPF Até',
+      gridColumns: 6
+    }
+  ];
+
 
   addEmployee() {
     alert('Cliquei no item do menu Incluir');
   }
 
+  onQuickSearch(value: any) {
+    console.log('onQuickSearch', value);
+  }
+
+  onAdvancedSearch(value: any) {
+    console.log('onAdvancedSearch', value);
+  }
+
+  onChangeDisclaimers(value: any) {
+    console.log('onChangeDisclaimers', value);
+  }
 }
